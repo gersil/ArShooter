a6e2b643ebb496f55c734268ec3f652d0bf2b381
gersil <gperezmari@hotmail.com>
2022-12-13 17:26:52 +0100

    Avance

--- Scripts/SpawnScript.cs
+++ Scripts/SpawnScript.cs
@@ -11,7 +11,19 @@ public class SpawnScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(4);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(ducks[i], spawnPoints[i].position, spawnPoints[i].rotation);
+        }
+
+        StartCoroutine(StartSpawning());
     }
   
 }
