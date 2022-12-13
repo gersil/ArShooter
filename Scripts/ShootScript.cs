d6cf8c83a66aeed6e88410a5825eeb5a0fc1a8cc
gersil <gperezmari@hotmail.com>
2022-12-13 19:12:54 +0100

    Aim

---
+++ Scripts/ShootScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+
+    public GameObject arCamera;
+    public GameObject smoke;
+    // Start is called before the first frame update
+    public void Shoot()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            if(hit.transform.name == "RubberDuck_Pond_MOBILE" || hit.transform.name == "RubberDuck_White_MOBILE" || hit.transform.name == "RubberDuck_Yellow_MOBILE")
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+
+            }
+        }
+    }
+
+}
